ad5f3c5350e6056bdcad6fef0ed38a038b5f0836
12210012 <12210012.gcit@rub.edu.bt>
2023-04-27 15:21:35 +0000

    Updated DriverSignup

--- controller/viewController.js
+++ controller/viewController.js
@@ -4,3 +4,11 @@ const path = require('path')
 exports.getSignupForm = (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'signup.html'))
 }
+
+exports.getDriverSignUp = (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'driver_signup.html'))
+}
+
+exports.getHome= (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'home.html'))
+}
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const viewsController = require('./../controller/viewController')
 
+router.get('/', viewsController.getHome)
 router.get('/signup', viewsController.getSignupForm)
+router.get('/driver', viewsController.getDriverSignUp)
+
 
 
 module.exports = router
